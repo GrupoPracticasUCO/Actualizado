d37ac0e3b49610f7c943ce257e6706f8caba0601
PracticasPW <56724926+PracticasPW@users.noreply.github.com>
2019-11-18 22:49:59 +0100

    actualizado el proyecto

---
+++ js/functionheader.js
@@ -0,0 +1,44 @@
+
+var paginas = ["Categorias","MisProyectos","Nuevo_Proyecto","Detalle_Proyecto","Donar","Contactar","Faq"];
+var pag, nuevo;
+
+window.onload = function(){
+	pag = String(window.location);// se queda
+	nuevo = extraerPagina(pag);
+	console.log("nuevo " + nuevo);
+	if(!addButton(nuevo,paginas)){
+		document.getElementById("titulo").style.width = "55%";
+	}
+};
+
+function addButton(nuevo,pagina){
+	var found = false;
+	
+	var contenido = document.createTextNode("Mi cuenta / Registrarse");
+	var a_element = document.createElement("a");
+	a_element.appendChild(contenido);
+	var h_ref = document.createAttribute("href");
+	h_ref.value = "#";
+	var id_a = document.createAttribute("id");
+	id_a.value = "acceso";
+	a_element.setAttributeNode(id_a);
+	a_element.setAttributeNode(h_ref);
+	var div_element = document.createElement("div");
+	div_element.appendChild(a_element);
+
+	for (var i = 0; i < pagina.length && !found; i++) {
+		if(nuevo === pagina[i]){
+			document.getElementsByClassName("cab-der")[0].appendChild(div_element);
+			found = true;
+		}
+	}
+	return found;
+}
+
+function extraerPagina(cadena){
+	pos = cadena.lastIndexOf("/");
+	pos2 = cadena.lastIndexOf(".");
+	pos3 = pos2 - pos -1;	
+	cad = cadena.substr(pos+1,pos3);
+	return cad;
+}
