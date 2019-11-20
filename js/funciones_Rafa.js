5c8f355d96599a50239847ba4b6f0858ee63266d
Rafa <i72hocar@uco.es>
2019-11-20 21:27:44 +0100

    acsi final Rafa

--- js/funciones_Rafa.js
+++ js/funciones_Rafa.js
@@ -1,4 +1,15 @@
+var paginas = ["Categorias","MisProyectos","Nuevo_Proyecto","Detalle_Proyecto","Donar","Contactar","Faq"];
+
+var nuevo;
 window.onload = function(){
+  pag = String(window.location);// se queda
+	nuevo = extraerPagina(pag);
+
+	if(!addButton(nuevo,paginas)){
+		document.getElementById("titulo").style.width = "55%";
+		var remover = document.getElementsByClassName("cab-der");
+		document.getElementById("cabecera").removeChild(remover[0]);
+	}
   document.getElementById("EnviarAcceder").addEventListener("click",function(){
     contrasena=document.getElementById("ContrasenaAcceder").value;
     fallo=validarContrasena(contrasena);
